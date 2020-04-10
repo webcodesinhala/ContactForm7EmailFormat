4e00a1be917868a0befd2f79dbfdadfd7353082e
webcodesinhala <63422918+webcodesinhala@users.noreply.github.com>
2020-04-11 00:28:27 +0545

    Create emailTemplate.php

---
+++ emailTemplate.php
@@ -0,0 +1,30 @@
+<table style="width:800px" border="1" cellpadding="0" cellspacing="0">
+    <tbody>
+        <tr>
+            <td colspan="2"><img src="http://localhost/webcodesinhala/wp-content/uploads/2020/04/email_header_new.jpg" alt="Email" class="CToWUd"></td>
+        </tr>
+        <tr>
+            <td style="padding:5px">Name:</td>
+            <td style="padding:5px">[your-name]</td>
+        </tr>
+        <tr>
+            <td style="padding:5px">E-mail:</td>
+            <td style="padding:5px">[your-email]</td>
+        </tr>
+        <tr>
+            <td style="padding:5px">Phone:</td>
+            <td style="padding:5px">[your-phone]</td>
+        </tr>
+        <tr>
+            <td style="padding:5px">Subject:</td>
+            <td style="padding:5px">[your-subject]</td>
+        </tr>
+        <tr>
+            <td style="padding:5px">Message:</td>
+            <td style="padding:5px">[your-message]</td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center" style="padding:5px">Copyright &copy; 2020 by Web Code Sinhala</td>
+        </tr>
+    </tbody>
+</table>
